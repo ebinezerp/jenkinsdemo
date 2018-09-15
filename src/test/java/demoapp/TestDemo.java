767ce23c4587e35fb2d054a7a87a3a06fbc76da0
ebinezer.p87@gmail.com <ebinezer.p87@gmail.com>
2018-09-15 10:19:11 +0530

    local commit

---
+++ src/test/java/demoapp/TestDemo.java
@@ -0,0 +1,24 @@
+package demoapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestDemo {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals(true,true);
+	}
+
+}
